de19999c77bfecd3b68a5e3f86c6de123e1d0d1b
李昀宸 <liyunchen@liyunchendeAir.home>
2023-10-13 04:26:01 +0800

    chore(dev): configure prettier & eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,81 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'simple-import-sort',
+    'no-relative-import-paths',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          // External packages
+          ['^@?\\w'],
+
+          // Constants
+          ['env', 'AppConstants', '^.*/constants$'],
+
+          // Type imports
+          ['^.*\\u0000$'],
+
+          // Internal packages inside `src` folder
+          [
+            '^(components|context|hooks|icons|layout|lib|modules|services|shared|theme|utils)(/.*|$)',
+          ],
+
+          // Side effect imports
+          ['^\\u0000'],
+
+          // Parent imports; put `..` last
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+
+          // Other relative imports; put same folder imports and `.` last
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+          // Style imports
+          ['^.+\\.s?css$'],
+
+          // Static assets
+          ['^(static)(/.*|$)'],
+        ],
+      },
+    ],
+    'no-relative-import-paths/no-relative-import-paths': [
+      'warn',
+      { allowSameFolder: true, prefix: '@' },
+    ],
+  },
+};
