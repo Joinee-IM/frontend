52952096a15d19e8042ee132a758f790826df3be
李昀宸 <liyunchen@liyunchendeAir.home>
2023-11-24 20:57:48 +0800

    fix(button): resolve type issue

--- src/modules/auth/pages/Login.tsx
+++ src/modules/auth/pages/Login.tsx
@@ -7,14 +7,20 @@ import Google from '@/assets/google.png';
 import { RippleButton } from '@/components';
 import AuthButton from '@/components/Button/AuthButton';
 import Divider from '@/components/Divider';
-import useError from '@/hooks/useError';
 import Card from '@/modules/auth/components/Card';
-import { useGoogleLogin, useLogin } from '@/modules/auth/service';
+import { useLogin } from '@/modules/auth/service';
 
 interface FieldType {
   email?: string;
   password?: string;
 }
+
+const layout = {
+  wrapperCol: { span: 32 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 0, span: 24 },
+};
 
 const ForgotPasswordWrapper = styled.div`
   width: 100%;
@@ -23,104 +29,58 @@ const ForgotPasswordWrapper = styled.div`
   justify-content: end;
 `;
 
-const TESTVALUE = {
-  email: 'b09705017@ntu.edu.tw',
-  password: 'string',
-};
-
 export default function Login() {
   const [form] = Form.useForm();
-  const { mutate: login, error, isLoading } = useLogin();
-  const { googleLogin } = useGoogleLogin();
-
+  const { mutate } = useLogin();
   const navigate = useNavigate();
-
-  const { context } = useError(error, undefined, () => {
-    if (error?.message === 'LoginFailed')
-      form.setFields([
-        {
-          name: 'email',
-          errors: [''],
-        },
-        {
-          name: 'password',
-          errors: [''],
-        },
-      ]);
-  });
 
   const handleButtonPress = (values: FieldType) => {
     const { email, password } = values;
-    if (email && password)
-      login(
-        { email, password },
-        {
-          onSuccess() {
-            navigate('/');
-          },
-        },
-      );
+    if (email && password) mutate({ email, password });
+    // alert(`${email}, ${password}`);
   };
 
   return (
-    <>
-      {context}
-      <Card>
-        <Form
-          form={form}
-          onFinish={handleButtonPress}
-          size={'middle'}
-          style={{ width: '100%' }}
-          initialValues={TESTVALUE}
-        >
-          <Form.Item name="email" rules={[{ required: true, message: '' }]}>
-            <Input prefix={<UserOutlined />} placeholder="電子郵件" autoComplete="off" />
-          </Form.Item>
-          <Form.Item
-            extra={
-              <ForgotPasswordWrapper>
-                <RippleButton
-                  category="link"
-                  palette="main"
-                  style={{ fontSize: '12px', fontWeight: 400, alignSelf: 'flex-end' }}
-                  onClick={() => navigate('/auth/forget-password')}
-                >
-                  忘記密碼？
-                </RippleButton>
-              </ForgotPasswordWrapper>
-            }
-            name="password"
-            rules={[{ required: true, message: '' }]}
-          >
-            <Input.Password prefix={<LockOutlined />} placeholder="密碼" autoComplete="off" />
-          </Form.Item>
-          <Form.Item>
+    <Card>
+      <Form
+        {...layout}
+        form={form}
+        name="control-hooks"
+        onFinish={handleButtonPress}
+        size={'middle'}
+        style={{ width: '100%' }}
+      >
+        <Form.Item name="email" rules={[{ required: true, message: '' }]}>
+          <Input prefix={<UserOutlined />} placeholder="電子郵件" autoComplete="off" />
+        </Form.Item>
+        <Form.Item name="password" rules={[{ required: true, message: '' }]}>
+          <Input.Password prefix={<LockOutlined />} placeholder="密碼" autoComplete="off" />
+          <ForgotPasswordWrapper>
             <RippleButton
-              category="solid"
+              category="link"
               palette="main"
-              htmlType="submit"
-              borderBox={true}
-              style={{ width: '100%' }}
-              loading={isLoading}
+              style={{ fontSize: '12px', fontWeight: 400, alignSelf: 'flex-end' }}
             >
-              登入
+              忘記密碼？
             </RippleButton>
-          </Form.Item>
-        </Form>
-        <Divider text="或是" />
-        <AuthButton image={Google} onClick={() => googleLogin()}>
-          以 Google 帳號登入
-        </AuthButton>
-        <RippleButton
-          type="link"
-          category="link"
-          palette="main"
-          onClick={() => navigate('/auth/signup/choose-role')}
-          style={{ fontSize: '14px', fontWeight: 400, marginTop: '50px' }}
-        >
-          還沒有帳號嗎？點我註冊
-        </RippleButton>
-      </Card>
-    </>
+          </ForgotPasswordWrapper>
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <RippleButton category="solid" palette="main" htmlType="submit" style={{ width: '100%' }}>
+            登入
+          </RippleButton>
+        </Form.Item>
+      </Form>
+      <Divider text="或是" />
+      <AuthButton image={Google}>以 Google 帳號登入</AuthButton>
+      <RippleButton
+        category="link"
+        palette="main"
+        onClick={() => navigate('/auth/signup/choose-role')}
+        style={{ fontSize: '14px', fontWeight: 400, marginTop: '50px' }}
+      >
+        還沒有帳號嗎？點我註冊
+      </RippleButton>
+    </Card>
   );
 }
