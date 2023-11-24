efa28d1ff9cc1b1e8b10a0a202124af775d09651
李昀宸 <liyunchen@liyunchendeAir.home>
2023-11-25 02:45:23 +0800

    feat(user-info): implement upload UI

--- src/modules/main/pages/UserInfo/components/Section.tsx
+++ src/modules/main/pages/UserInfo/components/Section.tsx
@@ -18,7 +18,7 @@ const SectionTitleWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  border-bottom: 1px solid #d8d8d8;
+  border-bottom: 1px solid ${({ theme }) => theme.gray[500]};
   font-size: 15px;
   font-weight: 500;
   padding: 0 0 3px 0;
