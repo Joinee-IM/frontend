370bcdb4633c87a931e409dfb4a6e92c628a5164
李昀宸 <liyunchen@liyunchendeAir.home>
2023-11-25 09:40:33 +0800

    feat(stadium): initialize google map layout

--- src/provider/routes.tsx
+++ src/provider/routes.tsx
@@ -22,22 +22,10 @@ const router = createBrowserRouter([
     children: [
       {
         path: routePath('index.stadium'),
-        children: [
-          {
-            index: true,
-            async lazy() {
-              const { Stadium: Component } = await import('@/modules/main/pages');
-              return { Component };
-            },
-          },
-          {
-            path: routePath('index.stadium.venue'),
-            async lazy() {
-              const { Venue: Component } = await import('@/modules/main/pages');
-              return { Component };
-            },
-          },
-        ],
+        async lazy() {
+          const { Stadium: Component } = await import('@/modules/main/pages');
+          return { Component };
+        },
       },
       {
         path: routePath('index.user-info'),
