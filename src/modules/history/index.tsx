9f607ea0174e8b2daef4d70bc9dc7ffb2863d871
賴羿蓁 <laiyichen@laiyizhendeMacBook-Air.local>
2023-12-20 20:42:23 +0800

    fix(history page): edit the history page style

--- src/modules/history/index.tsx
+++ src/modules/history/index.tsx
@@ -3,14 +3,16 @@ import { format } from 'date-fns';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import type { schemas } from '@/services/type';
-import type { StatusType } from '@/utils/function/map/toStatus';
+import type { StatusType } from '@/utils/function/map';
 import type { ColumnsType } from 'antd/es/table';
 import type { z } from 'zod';
 
 import { DirectionRightIcon } from '@/assets/icons/Direction';
+import { RoundTag, RoundTagWrapper } from '@/components/Tag';
+import { PageTitle } from '@/components/Title';
 import { useBrowseHistory } from '@/modules/history/services';
 import { Container } from '@/modules/main/components';
-import { toStatus } from '@/utils/function/map';
+import theme from '@/provider/theme/theme';
 
 interface DataType extends z.infer<(typeof schemas)['ViewMyReservation']> {
   key: React.Key;
@@ -19,7 +21,7 @@ interface DataType extends z.infer<(typeof schemas)['ViewMyReservation']> {
 export default function History() {
   const { account_id } = useParams();
   const { histories } = useBrowseHistory({
-    limit: 10,
+    limit: 30,
     offset: 0,
     account_id: Number(account_id),
     sort_by: 'time',
@@ -52,7 +54,31 @@ export default function History() {
     {
       title: '我的角色',
       dataIndex: 'is_manager',
-      render: (_, { is_manager }) => <div>{is_manager ? '主揪' : '成員'}</div>,
+      render: (_, { is_manager }) => (
+        <div>
+          {is_manager ? (
+            <RoundTagWrapper>
+              <RoundTag
+                style={{
+                  backgroundColor: theme.pink[500],
+                }}
+              >
+                主揪
+              </RoundTag>
+            </RoundTagWrapper>
+          ) : (
+            <RoundTagWrapper>
+              <RoundTag
+                style={{
+                  backgroundColor: theme.sub[300],
+                }}
+              >
+                成員
+              </RoundTag>
+            </RoundTagWrapper>
+          )}
+        </div>
+      ),
       sorter: (a, b) => Number(a.is_manager) - Number(b.is_manager),
       filters: [
         {
@@ -75,7 +101,51 @@ export default function History() {
     {
       title: '狀態',
       dataIndex: 'status',
-      render: (text) => <div>{toStatus(text as StatusType)}</div>,
+      // render: (text) => <div>{toStatus(text as StatusType)}</div>,
+      render: (text) => (
+        <div>
+          {(text as StatusType) === 'CANCELLED' ? (
+            <RoundTagWrapper>
+              <RoundTag
+                style={{
+                  backgroundColor: 'white',
+                  color: theme.gray[500],
+                  borderStyle: 'solid',
+                  borderColor: theme.gray[500],
+                }}
+              >
+                已取消
+              </RoundTag>
+            </RoundTagWrapper>
+          ) : (text as StatusType) === 'FINISHED' ? (
+            <RoundTagWrapper>
+              <RoundTag
+                style={{
+                  backgroundColor: 'white',
+                  color: theme.brown,
+                  borderStyle: 'solid',
+                  borderColor: theme.brown,
+                }}
+              >
+                已結束
+              </RoundTag>
+            </RoundTagWrapper>
+          ) : (
+            <RoundTagWrapper>
+              <RoundTag
+                style={{
+                  backgroundColor: 'white',
+                  color: theme.sub[500],
+                  borderStyle: 'solid',
+                  borderColor: theme.sub[500],
+                }}
+              >
+                未結束
+              </RoundTag>
+            </RoundTagWrapper>
+          )}
+        </div>
+      ),
       sorter: (a, b) => a.status.localeCompare(b.status),
     },
     {
@@ -83,7 +153,7 @@ export default function History() {
       key: 'x',
       render: (_, record) => (
         <a onClick={() => navigate(`/reservation/info/${record.reservation_id}`)}>
-          <DirectionRightIcon />
+          <DirectionRightIcon style={{ color: theme.gray[500] }} />
         </a>
       ),
     },
@@ -96,6 +166,7 @@ export default function History() {
 
   return (
     <Container>
+      <PageTitle>歷史紀錄</PageTitle>
       <Table dataSource={data} columns={columns}></Table>
     </Container>
   );
